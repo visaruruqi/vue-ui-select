da48c00dbd88010bec14b71e81a30e0f026755f9
Visar Uruqi <visar.uruqi@gmail.com>
2026-03-08 12:25:51 +0100

    add support for playground build

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -4,12 +4,17 @@ import { resolve } from 'path'
 
 export default defineConfig({
   root: resolve(__dirname),
+  base: '/',
   plugins: [vue()],
   resolve: {
     alias: {
       '@': resolve(__dirname, '../src'),
       'vue-ui-select': resolve(__dirname, '../src/index.ts'),
     },
+  },
+  build: {
+    outDir: resolve(__dirname, '../dist-playground'),
+    emptyOutDir: true,
   },
   server: {
     port: 5173,
